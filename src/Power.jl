66731b520a5ff00af806e438cd696db15a1813a1
mekise <mekise@users.noreply.github.com>
2022-07-11 23:31:46 +0100

    Change dblquad to hcubature integration + clean functions

--- src/Power.jl
+++ src/Power.jl
@@ -1,13 +1,17 @@
 function integrand(x, ϕinput, scattpos, alphas, ω, J::Stdd; normalized=false, imagshift=1E-12)
-    (r, θ, ϕ) = (x[1], x[2], x[3])
-    grad = complexgrad(x, ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)
-    arg = conj(totfieldpolar(x, ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift))*[1, 1/r, 1/r/sin(θ)].*grad
-    return imag(arg)*(r^2)*sin(θ)
+    grad = x -> complexder(x, ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)
+    arg = x -> conj(totfieldpolar(x, ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift))*grad(x)
+    return imag(arg(x))*(x[1]^2)*sin(x[2])
 end
 
+# function powerout(r, ϕinput, scattpos, alphas, ω, J::Stdd; normalized=false, imagshift=1E-12)
+#     f(ϕ, θ) = integrand([r, θ, ϕ], ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)[1]
+#     return dblquad(f, 0, 2π, θ->0, θ->π)
+# end
+
 function powerout(r, ϕinput, scattpos, alphas, ω, J::Stdd; normalized=false, imagshift=1E-12)
-    f(ϕ, θ) = integrand([r, θ, ϕ], ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)[1]
-    return dblquad(f, 0, 2π, θ->0, θ->π)
+    f(x) = integrand([r, x[1], x[2]], ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)
+    return hcubature(f, [0, 0], [π, 2π])
 end
 
 #########################
