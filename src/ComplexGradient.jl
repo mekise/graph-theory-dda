280e8cb2ac6660df509b959670aa6e7a7d3e6ccb
mekise <mekise@users.noreply.github.com>
2022-07-11 23:31:14 +0100

    Add compleder function to perform only derivative in r

--- src/ComplexGradient.jl
+++ src/ComplexGradient.jl
@@ -5,3 +5,12 @@ function complexgrad(x, ϕinput, scattpos, alphas, ω, J::Stdd; normalized=false
     gradimag = x -> ForwardDiff.gradient(fimag, x)
     return gradreal(x).+1im*gradimag(x)
 end
+
+function complexder(x, ϕinput, scattpos, alphas, ω, J::Stdd; normalized=false, imagshift=1E-12)
+    (r, θ, ϕ) = (x[1], x[2], x[3])
+    freal = r -> real(totfieldpolar([r, θ, ϕ], ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift))
+    fimag = r -> imag(totfieldpolar([r, θ, ϕ], ϕinput, scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift))
+    gradreal = r -> ForwardDiff.derivative(freal, r)
+    gradimag = r -> ForwardDiff.derivative(fimag, r)
+    return gradreal(r).+1im*gradimag(r)
+end
