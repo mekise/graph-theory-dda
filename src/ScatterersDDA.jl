46a6eed959eba69dbea62b0e5a680b6ce1fa6d24
Stefano Scali <scali.stefano@gmail.com>
2022-07-11 11:14:04 +0100

    Update module package calls

--- src/ScatterersDDA.jl
+++ src/ScatterersDDA.jl
@@ -4,10 +4,13 @@ using LinearAlgebra
 using SpecialFunctions
 using Random
 using ForwardDiff
+using MultiQuad
 
-include("Misc.jl")
+include("Misc.jl");
 include("GreensFunction.jl");
-include("InteractionMatrix.jl")
-include("Fields.jl")
+include("InteractionMatrix.jl");
+include("Fields.jl");
+include("ComplexGradient.jl")
+include("Power.jl")
 
 end
