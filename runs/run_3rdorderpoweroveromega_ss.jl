9673e3bccb2b662db593b555f5c4edee3bf83b5c
Stefano Scali <scali.stefano@gmail.com>
2022-08-15 19:54:24 +0100

    New plot for 3rd order EP and comment on alphas parameters

--- runs/run_3rdorderpoweroveromega_ss.jl
+++ runs/run_3rdorderpoweroveromega_ss.jl
@@ -16,23 +16,26 @@ scattpos = [[-π 0. 0.]
 ϕinput = rand(3).+rand(3).*1im
 maxradius = maximum([norm(scattpos[i, :]) for i in 1:length(scattpos[:, 1])])
 
+###################
+### NB: there is a conj() in alphas[2] due to inconsistency with the matrix from mathematica.
+###     With this conj() the 2 expressions coincide and we find the EP.
 G12 = 1/8/π^2
-alphas = [(1/2)*(G12/Complex(2*sqrt(2)*G12^6 + 3*G12^6)^(1/3) + ((2*sqrt(2) + 3)^(1/3)*Complex(G12^6)^(1/3))/G12^3 + 1/G12)
-          -(-2 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 + Complex(G12^6)^(2/3)/((3 + 2*sqrt(2))^(1/3)*G12^4) + 
-            sqrt(6 - (3*Complex(G12^6)^(1/3))/((3 + 2*sqrt(2))^(2/3)*G12^2) - (3*Complex(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4))/(4*G12)
-          (2 - ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 - G12^2/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3) + 
+alphas = [(1/2)*(1/G12 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^3 + G12/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3))
+          conj(-((-2 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 + Complex(G12^6)^(2/3)/((3 + 2*sqrt(2))^(1/3)*G12^4)
+            + sqrt(6 - (3*Complex(G12^6)^(1/3))/((3 + 2*sqrt(2))^(2/3)*G12^2) - (3*Complex(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4))/(4*G12)))
+          (2 - ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 - G12^2/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3) +
             sqrt(6 - (3*(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4 - (3*G12^4)/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(2/3)))/(4*G12)]
-
+###################
 
 ## Evaluation ##
 ϵ = 0.1
-ωspan = LinRange(1-ϵ, 1+ϵ, 10)
+ωspan = LinRange(1-ϵ, 1+ϵ, 1000)
 Pout = zeros(length(ωspan))
 p = Progress(length(ωspan))
 
 println("Starting...")
 Threads.@threads for i in eachindex(ωspan)
-    Pout[i] = powerout(maxradius, ϕinput, scattpos, alphas, ωspan[i], J; normalized=normalized, imagshift=0)[1]
+    Pout[i] = powerout(maxradius, ϕinput, scattpos, alphas, ωspan[i], J; normalized=normalized)[1]
     next!(p)
 end
 
