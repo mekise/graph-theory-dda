baa70cde2d838325167be317a88b81a7915fc613
Stefano Scali <scali.stefano@gmail.com>
2022-08-15 23:06:32 +0100

    Build animation to scan EP in 3x3 + save gif

--- runs/run_3rdordertotalfieldGIF.jl
+++ runs/run_3rdordertotalfieldGIF.jl
@@ -10,12 +10,25 @@ normalized = false
 
 ## EP parameters ##
 ϵ = 0.05
-ωspan = LinRange(ω-ϵ, ω+ϵ, 100)
+ωspan = LinRange(ω-ϵ, ω+ϵ, 200)
+
+## EP parameters ##
 scattpos = [[-π 0. 0.]
-            [0. 0. 0.] # changing the y location of the central scatterer does not change the resulting output power
+            [0. sqrt(3)*π 0.] # equidistant scatterers with G = 1/8/π^2
             [π 0. 0.]]
 ϕinput = rand(3).+rand(3).*1im
 maxradius = maximum([norm(scattpos[i, :]) for i in 1:length(scattpos[:, 1])])
+
+###################
+### NB: there is a conj() in alphas[2] due to inconsistency with the matrix from mathematica.
+###     With this conj() the 2 expressions coincide and we find the EP.
+G12 = 1/8/π^2
+alphas = [(1/2)*(1/G12 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^3 + G12/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3))
+          conj(-((-2 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 + Complex(G12^6)^(2/3)/((3 + 2*sqrt(2))^(1/3)*G12^4)
+            + sqrt(6 - (3*Complex(G12^6)^(1/3))/((3 + 2*sqrt(2))^(2/3)*G12^2) - (3*Complex(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4))/(4*G12)))
+          (2 - ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 - G12^2/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3) +
+            sqrt(6 - (3*(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4 - (3*G12^4)/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(2/3)))/(4*G12)]
+###################
 
 ## Evaluation ##
 xx = LinRange(-10, 10, 200)
@@ -25,12 +38,9 @@ p = Progress(length(xx));
 for k in eachindex(ωspan)
     for i in eachindex(xx)
         Threads.@threads for j in eachindex(yy)
-            alphas = [α(ωspan[k], 1, 1/(4*sqrt(2)*π^2)) # it should be α(1, 1, 1/(4*sqrt(2)*π^2))
-                      α(ωspan[k], 1, 0.00000001)
-                      conj(α(ωspan[k], 1, 1/(4*sqrt(2)*π^2)))]
-            phitot[j, i] = totfield([xx[i], yy[j], 0], ϕinput, scattpos, alphas, ω, J; normalized=normalized)
+            phitot[j, i] = totfield([xx[i], yy[j], 0], ϕinput, scattpos, alphas, ωspan[k], J; normalized=normalized)
         end
     end
     next!(p)
-    npzwrite("./gif/gif"*string(k)*".npz", Dict("scattpos" => scattpos, "xx" => xx, "yy" => yy, "phitot" => phitot))
+    npzwrite("./gif/gif"*string(k)*".npz", Dict("omegaspan" => ωspan, "phiinput" => ϕinput, "scattpos" => scattpos, "xx" => xx, "yy" => yy, "phitot" => phitot))
 end
