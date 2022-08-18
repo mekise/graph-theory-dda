87739bf86256a013aac21171cd9edfe0bcb43b0a
Stefano Scali <scali.stefano@gmail.com>
2022-08-18 13:11:05 +0100

    Change and replot all eigs coalesce to optimize npz outputs

--- runs/run_eigcoalescence_3scatt_GIF.jl
+++ runs/run_eigcoalescence_3scatt_GIF.jl
@@ -14,6 +14,12 @@ rover2 = π
 steps = 400
 rspan = LinRange(sqrt(3)*rover2-ϵ, sqrt(3)*rover2+ϵ, steps)
 
+scattpos = zeros((steps, 3, 3))
+for i in 1:steps
+    scattpos[i, :, :] = [[-rover2 0. 0.]
+                         [0. rspan[i] 0.] # equidistant scatterers with G = 1/8/π^2
+                         [rover2 0. 0.]]
+end
 ###################
 ### Evaluated using Mathematica symbolic solution
 ### NB: there is a conj() in alphas[2] due to inconsistency with the matrix from mathematica.
@@ -26,21 +32,15 @@ rspan = LinRange(sqrt(3)*rover2-ϵ, sqrt(3)*rover2+ϵ, steps)
 #             sqrt(6 - (3*(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4 - (3*G12^4)/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(2/3)))/(4*G12)]
 ###################
 ### Evaluated using NLsolve solution
-alphas = [-7.013368475092600482445835849401992129215372596298039756222201423284825304069556 - 42.53024981045743992016289461947802809987344773660556638804706482651633259017354im, 132.4619897632574997546732604945833408650191728587364235821477266247433660309102 + 7.006613806219794923619144866639183660810213598907112437214055568579551121144819e-30im, -7.013368475092600482445835849402012558805883329658692400789161106750101841856979 + 42.53024981045743992016289461947723489761815542995479655135939519941658644818625im]
+alphas = [-7.013368475092600482445835849401586148371126697377497396993147566773778372522662 + 42.53024981045743992016289461947781909647920721596898054239751469515387324972564im, -7.013368475092600482445835849401586148371126697377497396993147566773778372522731 - 42.53024981045743992016289461947781909647920721596898054239751469515387324972564im, 132.46198976325749975467326049460771358600803016240562244219341392091438694475 + 6.703134469499830002807082037260090296948103626424876531613289800474263330871294e-77im]
 ###################
 
 ## Evaluation ##
 p = Progress(length(rspan));
 eigs = zeros(Complex{Float64}, (steps, length(alphas)))
 Threads.@threads for k in eachindex(rspan)
-    scattpos = [[-rover2 0. 0.]
-                [0. rspan[k] 0.] # equidistant scatterers with G = 1/8/π^2
-                [rover2 0. 0.]]
-    eigs[k, :] = eigvals(intmatrix(scattpos, alphas, ω, J; normalized=normalized, imagshift=1E-23))
+    eigs[k, :] = eigvals(intmatrix(scattpos[k, :, :], alphas, ω, J; normalized=normalized, imagshift=1E-23))
     next!(p)
 end
 
-scattpos = [[-rover2 0. 0.]
-            [0. rspan[1] 0.] # equidistant scatterers with G = 1/8/π^2
-            [rover2 0. 0.]]
-npzwrite("./data/gif_nlsolve.npz", Dict("rspan" => rspan, "rover2" => float(rover2), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs))
+npzwrite("./data/eigcoalescence_3scatt.npz", Dict("rspan" => rspan, "rover2" => float(rover2), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs))
