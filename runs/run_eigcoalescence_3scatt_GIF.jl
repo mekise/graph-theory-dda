095b8ebb1af6d866a6396cf74249a8c63f480d22
Stefano Scali <scali.stefano@gmail.com>
2022-08-19 11:18:14 +0100

    Final version of eigs coalescence for different alphas prm + add tracing eigs in animation

--- runs/run_eigcoalescence_3scatt_GIF.jl
+++ runs/run_eigcoalescence_3scatt_GIF.jl
@@ -3,12 +3,14 @@ using .ScatterersDDA
 using LinearAlgebra
 using NPZ
 using ProgressMeter
+using NLsolve
 
 ω = 1.
 J = Stdd(1.)
 normalized = false
 
-## EP parameters ##
+###################
+### EP parameters
 ϵ = 0.02
 rover2 = π
 steps = 400
@@ -17,29 +19,59 @@ rspan = LinRange(sqrt(3)*rover2-ϵ, sqrt(3)*rover2+ϵ, steps)
 scattpos = zeros((steps, 3, 3))
 for i in 1:steps
     scattpos[i, :, :] = [[-rover2 0. 0.]
-                         [0. rspan[i] 0.] # equidistant scatterers with G = 1/8/π^2
+                         [0. rspan[i] 0.]
                          [rover2 0. 0.]]
 end
-###################
-### Evaluated using Mathematica symbolic solution
-### NB: there is a conj() in alphas[2] due to inconsistency with the matrix from mathematica.
-###     With this conj() the 2 expressions coincide and we find the EP.
-# G12 = 1/8/π^2
-# alphas = [(1/2)*(1/G12 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^3 + G12/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3))
-#           conj(-((-2 + ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 + Complex(G12^6)^(2/3)/((3 + 2*sqrt(2))^(1/3)*G12^4)
-#             + sqrt(6 - (3*Complex(G12^6)^(1/3))/((3 + 2*sqrt(2))^(2/3)*G12^2) - (3*Complex(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4))/(4*G12)))
-#           (2 - ((3 + 2*sqrt(2))^(1/3)*Complex(G12^6)^(1/3))/G12^2 - G12^2/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(1/3) +
-#             sqrt(6 - (3*(3 + 2*sqrt(2))^(2/3)*Complex(G12^6)^(2/3))/G12^4 - (3*G12^4)/Complex(3*G12^6 + 2*sqrt(2)*G12^6)^(2/3)))/(4*G12)]
 
-alphas = [132.46198976325752 + 0.0im
--7.0133684750926255 - 42.53024981045747im
-  -7.01336847509261 + 42.53024981045747im]
 ###################
-### Evaluated using NLsolve solution
-# alphas = [-7.013368475092600482445835849401586148371126697377497396993147566773778372522662 + 42.53024981045743992016289461947781909647920721596898054239751469515387324972564im, -7.013368475092600482445835849401586148371126697377497396993147566773778372522731 - 42.53024981045743992016289461947781909647920721596898054239751469515387324972564im, 132.46198976325749975467326049460771358600803016240562244219341392091438694475 + 6.703134469499830002807082037260090296948103626424876531613289800474263330871294e-77im]
-###################
+### Mathematica solutions 
+### full
+# alphas = [132.46198976325752 + 0.0im
+#           -7.0133684750926255 - 42.53024981045747im
+#           -7.01336847509261 + 42.53024981045747im]
+### truncated
+# alphas = [132.46 + 0.0im
+#           -7.01 - 42.53im
+#           -7.01 + 42.53im]
 
-## Evaluation ##
+###################
+### non-linear system solver
+G12 = 1/8/π^2
+
+function f!(F, x)
+    F[1] = -G12^3 + 1/(2*x[1]*x[2]*x[3])
+    F[2] = 3*G12^2 - (x[1]+x[2]+x[3])/(x[1]*x[2]*x[3])
+    F[3] = x[1] + x[2]*x[3]/(x[2]+x[3])
+end
+function j!(J, x)
+    J[1, 1] = -1/(2*x[1]^2*x[2]*x[3])
+    J[1, 2] = -1/(2*x[1]*x[2]^2*x[3])
+    J[1, 3] = -1/(2*x[1]*x[2]*x[3]^2)
+    J[2, 1] = (x[2]+x[3])/(x[1]^2*x[2]*x[3])
+    J[2, 2] = (x[1]+x[3])/(x[1]*x[2]^2*x[3])
+    J[2, 3] = (x[1]+x[2])/(x[1]*x[2]*x[3]^2)
+    J[3, 1] = 1
+    J[3, 2] = x[3]/(x[2]+x[3]) - x[2]*x[3]/(x[2]+x[3])^2
+    J[3, 3] = x[2]/(x[2]+x[3]) - x[2]*x[3]/(x[2]+x[3])^2
+end
+validsolution = "n"
+while validsolution != "y"
+    global s = nlsolve(f!, j!, 10*(rand(3)+rand(3)*1im), ftol=1e-20, iterations=1_000)
+    while s.f_converged == false
+        s = nlsolve(f!, j!, 10*(rand(3)+rand(3)*1im), ftol=1e-20, iterations=1_000)
+    end
+    s = nlsolve(f!, j!, s.zero, ftol=1e-80, iterations=10_000)
+    FF = [1. + 1im, 1., 1.]
+    f!(FF, s.zero)
+    println("sol = ", s.zero)
+    println("\nftol = " * string(s.ftol) * "\nresidual_norm = " * string(s.residual_norm) * "\nFF(alphas) = " * string(FF))
+    println("\nContinue with this solution? (y, n)")
+    global validsolution = readline()
+end
+alphas = s.zero
+
+###################
+### Evaluation
 p = Progress(length(rspan));
 eigs = zeros(Complex{Float64}, (steps, length(alphas)))
 Threads.@threads for k in eachindex(rspan)
@@ -47,4 +79,4 @@ Threads.@threads for k in eachindex(rspan)
     next!(p)
 end
 
-npzwrite("./data/eigcoalescence_3scatt_test.npz", Dict("rspan" => rspan, "rover2" => float(rover2), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs))
+npzwrite("./data/eigcoalescence_3scatt_nlsolve.npz", Dict("rspan" => rspan, "rover2" => float(rover2), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs))
