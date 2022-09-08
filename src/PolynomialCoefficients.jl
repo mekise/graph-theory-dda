67c7bb1a5b4bdeb569c54515d3111cce6be54ebe
Stefano Scali <scali.stefano@gmail.com>
2022-09-08 16:24:50 +0100

    Remove unused PolynomialCoefficients.jl

--- src/PolynomialCoefficients.jl
+++
@@ -1,6 +0,0 @@
-function buildCoefficients(scattpos, alphas, ω, J::Stdd; normalized=false, imagshift=1E-23)
-    n = size(scattpos, 1)
-    M = intmatrix(scattpos, alphas, ω, J; normalized=normalized, imagshift=imagshift)
-
-    res = λ
-end
