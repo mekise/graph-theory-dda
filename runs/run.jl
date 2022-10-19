c226322bfe17cd404355b2c911418a5e39ed5631
Stefano Scali <scali.stefano@gmail.com>
2022-10-19 16:13:07 +0100

    Move old runs to clean folder up

--- runs/run.jl
+++ runs/run.jl
@@ -8,9 +8,8 @@ using Combinatorics
 
 ω = 1.
 J = Stdd(1.)
-normalized = false
-nscatt = 7
-rsteps = 400 # around 40 for power output. around 400 for eigs coalescence
+nscatt = 5
+rsteps = 40 # around 40 for power output. around 400 for eigs coalescence
 
 # EP parameters
 ωshift = 0. # shift to lower the EP sensitivity and approach the inequality boundary to have a passive system
@@ -38,7 +37,7 @@ end
 # non-linear system solver
 function f!(F, x)
     for cindex in 1:nscatt
-        F[cindex] = sum(det.([intmatrix(scattpos[200, :, :], x, ω, J)[setdiff(1:end, i), setdiff(1:end, i)] for i in combinations([j for j in 1:nscatt], cindex-1)]))
+        F[cindex] = sum(det.([intmatrix(scattpos[200, :, :], x, ω+ωshift, J)[setdiff(1:end, i), setdiff(1:end, i)] for i in combinations([j for j in 1:nscatt], cindex-1)]))
     end
 end
 validsolution = "n"
@@ -58,32 +57,32 @@ end
 alphas = s.zero
 
 # Coalescence eigenvalues and eigenvectors
-eigs = zeros(ComplexF64, (rsteps, length(alphas)))
-eigvs = zeros(ComplexF64, (rsteps, length(alphas), length(alphas)))
-p = Progress(length(rspan));
-println("Starting...")
-Threads.@threads for k in eachindex(rspan)
-    eigs[k, :] = eigvals(intmatrix(scattpos[k, :, :], alphas, ω, J; normalized=normalized, imagshift=1E-23))
-    eigvs[k, :, :] = eigvecs(intmatrix(scattpos[k, :, :], alphas, ω, J; normalized=normalized, imagshift=1E-23))
-    next!(p)
-end
-npzwrite("./data/eigs_n7.npz", Dict("rspan" => rspan, "r" => float(r), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs, "eigvs" => eigvs))
-
-# Total field over r
-# xx = LinRange(-10, 10, 200)
-# yy = LinRange(-10, 10, 200)
-# phitot = zeros(ComplexF64, (rsteps, length(xx), length(yy)))
-# p = Progress(rsteps);
+# eigs = zeros(ComplexF64, (rsteps, length(alphas)))
+# eigvs = zeros(ComplexF64, (rsteps, length(alphas), length(alphas)))
+# p = Progress(length(rspan));
 # println("Starting...")
 # Threads.@threads for k in eachindex(rspan)
-#     for i in eachindex(xx)
-#         for j in eachindex(yy)
-#             phitot[k, j, i] = totfield([xx[i], yy[j], 0], ϕinput[k, :], scattpos[k, :, :], alphas, ω, J; normalized=normalized, imagshift=1E-23)
-#         end
-#     end
+#     eigs[k, :] = eigvals(intmatrix(scattpos[k, :, :], alphas, ω, J; imagshift=1E-23))
+#     eigvs[k, :, :] = eigvecs(intmatrix(scattpos[k, :, :], alphas, ω, J; imagshift=1E-23))
 #     next!(p)
 # end
-# npzwrite("./data/totalfield_n5_omegashift.npz", Dict("rspan" => rspan, "r" => float(r), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "phiinput" => ϕinput, "xx" => xx, "yy" => yy, "phitot" => phitot))
+# npzwrite("./data/eigs_n7.npz", Dict("rspan" => rspan, "r" => float(r), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "eigs" => eigs, "eigvs" => eigvs))
+
+# Total field over r
+xx = LinRange(-10, 10, 200)
+yy = LinRange(-10, 10, 200)
+phitot = zeros(ComplexF64, (rsteps, length(xx), length(yy)))
+p = Progress(rsteps);
+println("Starting...")
+Threads.@threads for k in eachindex(rspan)
+    for i in eachindex(xx)
+        for j in eachindex(yy)
+            phitot[k, j, i] = totfield([xx[i], yy[j], 0], ϕinput[k, :], scattpos[k, :, :], alphas, ω, J; imagshift=1E-23)
+        end
+    end
+    next!(p)
+end
+npzwrite("./data/totalfield_.npz", Dict("rspan" => rspan, "r" => float(r), "epsilon" => ϵ, "alphas" => alphas, "scattpos" => scattpos, "phiinput" => ϕinput, "xx" => xx, "yy" => yy, "phitot" => phitot))
 
 # Power output over r
 # maxradius = maximum([norm(scattpos[end, i, :]) for i in eachindex(scattpos[end, :, 1])])
